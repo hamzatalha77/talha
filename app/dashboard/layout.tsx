148b89b9bde46185fb1314b551b6c6db4753c511
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-19 12:25:50 +0000

    commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 export default async function DashboardLayout({
   children
 }: {
@@ -18,6 +19,10 @@ export default async function DashboardLayout({
 }) {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
+
+  if (!user) {
+    return redirect('/')
+  }
   return (
     <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
