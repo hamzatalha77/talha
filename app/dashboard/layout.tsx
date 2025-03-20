b3bbdc13a3d779cc64d4f79a80b9e7fd742875ca
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-20 13:39:07 +0000

    commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import DashboardNavigation from '../components/dashboard/DashboardNavigation'
 import { Button } from '@/components/ui/button'
@@ -61,6 +62,7 @@ export default async function DashboardLayout({
           </DropdownMenuContent>
         </DropdownMenu>
       </header>
+      {children}
     </div>
   )
 }
