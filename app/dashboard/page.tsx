3508221cf742356930e50039c034b43636a1d591
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-21 15:10:58 +0000

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const dashboard = () => {
-  return <div>DashboardNavigation</div>
+  return (
+    <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+      DashboardNavigation
+    </div>
+  )
 }
 
 export default dashboard
