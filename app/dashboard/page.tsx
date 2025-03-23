c8b5b35fcb07672d983df0f686fbbb4d2052acb1
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-23 15:30:40 +0000

    COMMIT

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { DollarSign } from 'lucide-react'
 import React from 'react'
 
@@ -7,10 +7,14 @@ const dashboard = () => {
   return (
     <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flxr-row items-center justify-between pb-2">
           <CardTitle>Total Revenue</CardTitle>
           <DollarSign className="h-4 w-4 text-green-500" />
         </CardHeader>
+        <CardContent>
+          <p className="text-2xl font-bold">$100.000</p>
+          <p className="text-xs text-muted-foreground Based on 100 Charges"></p>
+        </CardContent>
       </Card>
     </div>
   )
