b901158e581ec173374741f75271206258727b65
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-25 14:12:38 +0000

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ const dashboard = () => {
   return (
     <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
       <Card>
-        <CardHeader className="flex flxr-row items-center justify-between pb-2">
+        <CardHeader className="flex flex-row items-center justify-between pb-2">
           <CardTitle>Total Revenue</CardTitle>
           <DollarSign className="h-4 w-4 text-green-500" />
         </CardHeader>
@@ -17,19 +17,17 @@ const dashboard = () => {
         </CardContent>
       </Card>
       <Card>
-        <CardHeader className="flex flxr-row items-center justify-between pb-2">
+        <CardHeader className="flex flex-row items-center justify-between pb-2">
           <CardTitle>Total Sales</CardTitle>
           <ShoppingBag className="h-4 w-4 text-blue-500" />
         </CardHeader>
         <CardContent>
           <p className="text-2xl font-bold">+50</p>
-          <p className="text-xs text-muted-foreground Based on 100 Charges">
-            Total Sales on Talha
-          </p>
+          <p className="text-xs text-muted-foreground">Total Sales on Talha</p>
         </CardContent>
       </Card>
       <Card>
-        <CardHeader className="flex flxr-row items-center justify-between pb-2">
+        <CardHeader className="flex flex-row items-center justify-between pb-2">
           <CardTitle>Total Products</CardTitle>
           <PartyPopper className="h-4 w-4 text-indigo-500" />
         </CardHeader>
@@ -41,7 +39,7 @@ const dashboard = () => {
         </CardContent>
       </Card>
       <Card>
-        <CardHeader className="flex flxr-row items-center justify-between pb-2">
+        <CardHeader className="flex flex-row items-center justify-between pb-2">
           <CardTitle>Total Users</CardTitle>
           <User2 className="h-4 w-4 text-indigo-500" />
         </CardHeader>
