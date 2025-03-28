6f05a9370d4288e0b7f3fadaee7b03bbd7a146ff
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-28 14:08:27 +0000

    commit

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -5,6 +5,14 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
 import React from 'react'
 
 const OrdersPage = () => {
@@ -14,7 +22,33 @@ const OrdersPage = () => {
         <CardTitle>Orders</CardTitle>
         <CardDescription>Recent orders from your store!</CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <p className="font-medium">Hamza Talha</p>
+                <p className="hidden md:flex text-sm text-muted-foreground">
+                  hamza@gmail.com
+                </p>
+              </TableCell>
+              <TableCell>Sale</TableCell>
+              <TableCell>Successfull</TableCell>
+              <TableCell>2024-06-15</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
     </Card>
   )
 }
