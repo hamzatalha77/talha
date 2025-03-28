a60ae54872ff6c194941fdbea7f61ef5ccdb8d61
hamzatalha77 <hamza_ta7@hotmail.com>
2025-03-28 14:48:13 +0000

    commit

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const ProductsPage = () => {
-  return <div>hello</div>
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild className="flex items-center gap-x-2">
+          <Link href="/dashboard/products/create">
+            <PlusCircle className="w-3.5 h-3.5">
+              <span>Add Products</span>
+            </PlusCircle>
+          </Link>
+        </Button>
+      </div>
+    </>
+  )
 }
 
 export default ProductsPage
