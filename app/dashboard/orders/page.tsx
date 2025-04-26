45a83c56cd0b06b28a18bec72f3ce74bd878a48b
hamzatalha77 <hamza_ta7@hotmail.com>
2025-04-26 21:50:39 +0100

    commit

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -38,7 +38,7 @@ const OrdersPage = () => {
               <TableCell>
                 <p className="font-medium">Hamza Talha</p>
                 <p className="hidden md:flex text-sm text-muted-foreground">
-                  hamza@gmail.com
+                  hamza_ta7@gmail.com
                 </p>
               </TableCell>
               <TableCell>Sale</TableCell>
