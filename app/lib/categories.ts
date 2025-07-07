7dcdb7f14235991dcb37a89ca4c3fa925ceb717d
hamzatalha77 <hamza_ta7@hotmail.com>
2025-07-07 10:49:12 +0100

    Adds predefined category list for application use
    Introduces a static list of categories with IDs, titles, and names for use in the application. This provides a consistent structure for handling category-related functionality and improves maintainability.

---
+++ app/lib/categories.ts
@@ -0,0 +1,17 @@
+export const categories = [
+  {
+    id: 0,
+    title: 'Men',
+    name: 'men'
+  },
+  {
+    id: 1,
+    title: 'Woman',
+    name: 'woman'
+  },
+  {
+    id: 2,
+    title: 'Kids',
+    name: 'kids'
+  }
+]
